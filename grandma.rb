2d2bac079b30d752711653c6c7822bdc5452d961
Elijah wines <elijahwines84@yahoo.com>
2020-02-10 19:08:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+
+  elsif phrase == downcase
+    return "HUH?! SPEAK UP, SONNY!"
+    
   end
 end
